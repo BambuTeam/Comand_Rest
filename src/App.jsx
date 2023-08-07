1281e8b6ec3d16a3c284909cd4b8d64b8895d112
Bambu Team |  Luis Enrique Concuá <75746976+BambuTeam@users.noreply.github.com>
2023-08-07 11:37:23 -0600

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+
+function App() {
+ 
+
+  return (
+    <div className='bg-slate-100'>
+     Hola Muchachos Perrunos
+     <img src="https://em-content.zobj.net/source/skype/289/dog_1f415.png" alt="Gua!" />
+    
+    </div>
+  )
+}
+
+export default App  
